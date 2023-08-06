30f0338989f3ce057bf47116ae2ace415de1c0b9
Daud Hidayat R <daud28ramadhan@gmail.com>
2023-08-06 16:59:35 +0700

    membuat database
    Signed-off-by: Daud Hidayat R <daud28ramadhan@gmail.com>

--- database.sql
+++ database.sql
@@ -1,3 +1,12 @@
 show databases;
 create database belajar_laravel_database;
 use belajar_laravel_database;
+
+create table catagories(
+    id varchar(100) not null primary key,
+    name varchar(100) not null,
+    description text,
+    created_at timestamp default current_timestamp
+)engine innodb;
+desc catagories;
+
